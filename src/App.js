903c9377b22b1d2f3556f2d7ee14b2446a961b54
AxyseCom <axyse.com@gmail.com>
2020-12-22 21:51:45 +0300

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
+import {Container, Row, Col} from "react-bootstrap";
 
 function App() {
   return (
     <>
-      <h1>hi git</h1>
+      <Container>
+        <Row>
+          <Col>1</Col>
+          <Col>2</Col>
+          <Col>3</Col>
+        </Row>
+      </Container>
     </>
   );
 }
