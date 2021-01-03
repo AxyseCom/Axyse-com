4c962a256de37340d2c8af54796082fa210c39ef
AxyseCom <axyse.com@gmail.com>
2021-01-03 23:41:22 +0300

    added font for header

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import 'bootstrap/scss/bootstrap.scss';
 import "./styles/style.scss"
 import './styles/style.css'
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.js';
