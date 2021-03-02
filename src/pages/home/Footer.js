0115d8195ab74ec416e7b185572ce05202f9d741
AxyseCom <axyse.com@gmail.com>
2021-03-02 17:06:29 +0300

    added footer

--- src/pages/home/Footer.js
+++ src/pages/home/Footer.js
@@ -1,11 +1,47 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { Link, animateScroll as scroll } from "react-scroll";
+import Logo from "../../assets/images/logo-light.svg";
 
-export default function Footer(){
-    return(
-        <>
-        <footer>
-            
-        </footer>
-        </>
-    )
+function scrollToTop() {
+    scroll.scrollToTop();
+    const block = document.getElementById("res-block");
+    setTimeout(() => {
+      block.style.display = "none";
+    }, 1000);
+  }
+function getDate(){
+    const date = new Date();
+    return date.getFullYear().toString()
+}
+
+export default function Footer() {
+  return (
+    <>
+      <footer>
+        <Container>
+          <Row className="justify-content-md-center">
+            <Col style={{
+                height:"160px",
+                margin: "auto",
+                textAlign: "center"
+            }}>
+            <img
+                src={Logo}
+                height="70"
+                width="70"           
+                alt="Logo"
+                href="/home"
+                style={{
+                    marginTop:"30px"
+                }}
+                onClick={scrollToTop}
+              />
+              <p>© {getDate()} Axyse.com All Rights Reserved.</p>
+            </Col>
+          </Row>
+        </Container>
+      </footer>
+    </>
+  );
 }
