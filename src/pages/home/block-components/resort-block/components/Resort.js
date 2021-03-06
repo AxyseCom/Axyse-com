0bade0292b43d8ac1550fe497c424d62defedad9
AxyseCom <axyse.com@gmail.com>
2021-03-06 21:55:16 +0300

    added checkbox and styles

--- src/pages/home/block-components/resort-block/components/Resort.js
+++ src/pages/home/block-components/resort-block/components/Resort.js
@@ -1,32 +1,60 @@
 import React from "react";
 import { Component } from "react";
 import { Col, Container, Row, Accordion, Card, Button } from "react-bootstrap";
-import { Button as MaterialButton, ButtonGroup } from "@material-ui/core";
+import {
+  Button as MaterialButton,
+  ButtonGroup,
+  Checkbox,
+  FormControlLabel,
+} from "@material-ui/core";
 
 export default class Resort extends Component {
   constructor(props) {
     super(props);
     this.getPrice = this.getPrice.bind(this);
     this.deletePrice = this.deletePrice.bind(this);
+    this.handleChange = this.handleChange.bind(this);
     this.state = {
       day: "",
       price: "",
       currency: "",
       id: 0,
-      value: false,
+      checkAdult: true,
+      checkChildren: false,
     };
   }
   getPrice(event, day, price, currency) {
-    this.setState({ day: day, price: price, currency: currency, id: this.props.data.id });
+    this.setState({
+      day: day,
+      price: price,
+      currency: currency,
+      id: this.props.data.id,
+    });
   }
 
   deletePrice(event) {
-    this.setState({ day: "", price: "", currency: "", id: this.props.data.id });
+    this.setState({
+      day: "",
+      price: "",
+      currency: "",
+      id: this.props.data.id,
+      checkAdult: true,
+      checkChildren: false
+    });
+  }
+
+  handleChange(event) {
+    if (this.state.checkAdult == false) {
+      this.setState({ checkAdult: true, checkChildren: false });
+    } else {
+      this.setState({ checkAdult: false, checkChildren: true });
+    }
+    this.getPrice();
   }
 
   render() {
-    if(this.state.id != this.props.data.id){
-      this.deletePrice()
+    if (this.state.id != this.props.data.id) {
+      this.deletePrice();
     }
     return (
       <>
@@ -61,6 +89,29 @@ export default class Resort extends Component {
                 &nbsp;
                 {this.state.currency}
               </h5>
+
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={this.state.checkAdult}
+                    onChange={this.handleChange}
+                    name="checkedB"
+                    color="primary"
+                  />
+                }
+                label="Взрослый"
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={this.state.checkChildren}
+                    onChange={this.handleChange}
+                    name="checkedB"
+                    color="primary"
+                  />
+                }
+                label="Детский"
+              />
               <ButtonGroup
                 size="large"
                 color="primary"
@@ -70,12 +121,21 @@ export default class Resort extends Component {
                   return (
                     <MaterialButton
                       onClick={(e) => {
-                        this.getPrice(
-                          e,
-                          price.dayNumber,
-                          price.adultDay,
-                          price.currency
-                        );
+                        if (this.state.checkAdult == true) {
+                          this.getPrice(
+                            e,
+                            price.dayNumber,
+                            price.adultDay,
+                            price.currency
+                          );
+                        } else {
+                          this.getPrice(
+                            e,
+                            price.dayNumber,
+                            price.childDay,
+                            price.currency
+                          );
+                        }
                       }}
                     >
                       {price.dayNumber}
